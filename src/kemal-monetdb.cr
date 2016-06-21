10c2391711ccdb0b351921d215cbfa80b270970a
Bri in The Sky <brianh6854@googlemail.com>
2016-06-21 18:17:07 +0100

    Added git ignore / start point

---
+++ src/kemal-monetdb.cr
@@ -0,0 +1,36 @@
+require "mysql"
+require "pool/connection"
+require "http"
+
+macro conn
+  env.mysql.connection
+end
+
+macro release
+  env.mysql.release
+end
+
+def mysql_connect(options, capacity = 25, timeout = 0.1)
+  Kemal.config.add_handler Kemal::MySQL.new(options, capacity, timeout)
+end
+
+class HTTP::Server::Context
+  @mysql : ConnectionPool(MySQL::Connection) | Nil
+  property! mysql
+end
+
+class Kemal::MySQL < HTTP::Handler
+  @mysql : ConnectionPool(MySQL::Connection)
+  getter mysql
+
+  def initialize(options={} of String => String, capacity = 25, timeout = 0.1)
+    @mysql = ConnectionPool.new(capacity: capacity, timeout: timeout) do
+      ::MySQL.connect(options["host"], options["user"], options["password"], options["db"], 3306_u16, nil)
+    end
+  end
+
+  def call(context)
+    context.mysql = @mysql
+    call_next(context)
+  end
+end
---
+++ src/kemal-monetdb/version.cr
@@ -0,0 +1,3 @@
+module Kemal::MonetDB
+  VERSION = "0.1.2"
+end
